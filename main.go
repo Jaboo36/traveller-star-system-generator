6bcb6f353ca9bb7589e5cef089ed60eb18506702
Josh Waterhouse <josh@cordiance.com>
2026-03-26 16:08:48 -0700

    feat: create functions to determine temperature, population, government, factions, and cultural differences

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -41,11 +42,12 @@ func continueOrStop(scanner *bufio.Scanner) bool {
 	return continueOrStop
 }
 
-func roll2D() uint8 {
-	roll1 := uint8(rand.Intn(6) + 1)
-	roll2 := uint8(rand.Intn(6) + 1)
+func rollD() uint8 {
+	return uint8(rand.Intn(6) + 1)
+}
 
-	return roll1 + roll2
+func roll2D() uint8 {
+	return rollD() + rollD()
 }
 
 type planetSize struct {
@@ -134,13 +136,208 @@ func determineHydrographics(atmosphere uint8) hydrographics {
 	return hydrographicsProfiles[result]
 }
 
+type planetTemperature struct {
+	Type        string
+	AverageTemp string
+	Description string
+}
+
+func determineTemperature(atmosphere uint8) planetTemperature {
+	diceMod := map[uint8]int8{
+		0:   0,
+		1:   0,
+		2:   -2,
+		3:   -2,
+		4:   -1,
+		5:   -1,
+		0xE: -1,
+		6:   0,
+		7:   0,
+		8:   1,
+		9:   1,
+		0xA: 2,
+		0xD: 2,
+		0xF: 2,
+		0xB: 6,
+		0xC: 6,
+	}
+
+	switch result := int8(roll2D()) + diceMod[atmosphere]; {
+	case result <= 2:
+		return planetTemperature{"Frozen", "-51 degrees or less", "Frozen world. No liquid water, very dry atmosphere."}
+	case result < 5:
+		return planetTemperature{"Cold", "-51 degrees to 0 degrees", "Icy world. Little liquid water, extensive ice caps, few clouds."}
+	case result < 10:
+		return planetTemperature{"Temperate", "0 degrees to 30 degrees", "Temperate world. Earth-like. Liquid & vaporized water are common, moderate ice caps."}
+	case result < 12:
+		return planetTemperature{"Hot", "31 degrees to 80 degrees", "Hot world. Small or no ice caps, little liquid water. Most water in the form of clouds."}
+	default:
+		return planetTemperature{"Boiling", "81 degrees or more", "Boiling world. No ice caps, little liquid water."}
+	}
+}
+
+type population struct {
+	Id          uint8
+	Inhabitants string
+	Range       string
+	Description string
+}
+
+func determinePopulation() population {
+	populations := map[uint8]population{
+		0:   population{0, "None", "0", ""},
+		1:   population{1, "Few", "1+", "A tiny farmstead or a single family"},
+		2:   population{2, "Hundreds", "100+", "A village"},
+		3:   population{3, "Thousands", "1,000+", ""},
+		4:   population{4, "Tens of thousands", "10,000+", "Small town"},
+		5:   population{5, "Hundreds of thousands", "100,000+", "Average city"},
+		6:   population{6, "Millions", "1,000,000+", ""},
+		7:   population{7, "Tens of millions", "10,000,000+", "Large city"},
+		8:   population{8, "Hundreds of millions", "100,000,000+", ""},
+		9:   population{9, "Billions", "1,000,000,000", "Present day Earth"},
+		0xA: population{0xA, "Tens of billions", "10,000,000,000+", ""},
+		0xB: population{0xB, "Hundreds of billions", "100,000,000,000", "Incredibly crowded world"},
+		0xC: population{0xC, "Trillions", "1,000,000,000,000+", "World-city"},
+	}
+
+	return populations[roll2D()]
+}
+
+type government struct {
+	Id                uint8
+	governmentType    string
+	description       string
+	examples          string
+	exampleContraband []string
+}
+
+func determineGovernment(population uint8) government {
+	governments := map[uint8]government{
+		0:   government{0, "None", "No government structure. In many cases, family bonds predominate.", "Family, clan, anarchy", []string{}},
+		1:   government{1, "Company/Corporation", "Ruling functions are assumed by a company managerial elite and most citizenry are company employees or dependants.", "Corporate outpost, asteroid mine, feudal domain", []string{"Weapons", "Drugs", "Travellers"}},
+		2:   government{2, "Participating Democracy", "Ruling functions are reached by the advice and consent of the citizenry directly.", "Collective, tribal council, comm-linked consensus", []string{"Drugs"}},
+		3:   government{3, "Self-perpetuating Oligarchy", "Ruling functions are performed by a restricted minority, with little or no input from the mass of citizenry.", "Plutocracy, hereditary ruling caste", []string{"Technology", "Weapons", "Travllers"}},
+		4:   government{4, "Representative Democracy", "Ruling functions are performed by elected representatives.", "Republic, democracy", []string{"Drugs", "Weapons", "Psionics"}},
+		5:   government{5, "Feudal Technocracy", "Ruling functions are performed by specific individuals for persons who agree to be ruled by them. Relationships are based on the performance of technical activities that a mutually beneficial.", "Those with access to advanced technology tend to have higher social status", []string{"Technology", "Weapons", "Computers"}},
+		6:   government{6, "Captive Government", "Ruling functions are performed by an imposed leadership answerable to an outside group.", "A colony or conquered area", []string{"Weapons", "Technology", "Travellers"}},
+		7:   government{7, "Balkanisation", "No central authority exists; rival governments compete for control. Law level refers to the government nearest the starport.", "Multiple governments, civil war", []string{"Varies"}},
+		8:   government{8, "Civil Service Bureaucracy", "Ruling functions are performed by government agencies employing individuals selected to their expertise.", "Technocracy, Communism", []string{"Drugs", "Weapons"}},
+		9:   government{9, "Impersonal Bureaucracy", "Ruling functions are performed by agencies that have become insulated from the governed citizens.", "Entrenched castes of bureaucrats, decaying empire", []string{"Drugs", "Weapons"}},
+		0xA: government{0xA, "Charismatic Dictator", "Ruling functions are performed by agencies directed by a single leader who enjoys the overwhelming confidence of the citizens.", "Revolutionary leader, messiah, emperor", []string{}},
+		0xB: government{0xB, "Non-Charismatic Leader", "A previous charasmatic dictator has been replaced by a leader through normal channels.", "Military dictatorship, hereditary kingship", []string{"Weapons", "Technology", "Computers"}},
+		0xC: government{0xC, "Charismatic Oligarchy", "Ruling funtcions are performed by a select group of members of an organization or class that enjoys the overwhelming confidence of the citizenry.", "Junta, revolutionary council", []string{"Weapons"}},
+		0xD: government{0xD, "Religious Dictatorship", "Ruling functions are performed by a religious organization without regard to the specific individual needs of the citizenry.", "Cult, transcendent philosophy, psionic group mind", []string{"Varies"}},
+		0xE: government{0xE, "Religious Autocracy", "Government by a single religious leader having absolute power over the citizenry.", "Messiah", []string{"Varies"}},
+		0xF: government{0xF, "Totalitarian Oligarchy", "Government by an all-powerful minority which maintains absolute control through widespread coercion and oppression.", "World church, ruthless corporation", []string{"Varies"}},
+	}
+
+	return governments[roll2D()-7+population]
+}
+
+type faction struct {
+	GovernmentType   government
+	RelativeStrength string
+}
+
+func determineFactions(government uint8, population uint8) []faction {
+	var modifier int8
+	switch {
+	case government == 0 || government == 7:
+		modifier = 1
+	case government >= 10:
+		modifier = -1
+	default:
+		modifier = 0
+	}
+
+	numberOfFactions := int8(roll2D()) + int8(rollD()) + modifier
+
+	factions := []faction{}
+
+	for i := 0; i <= int(numberOfFactions); i++ {
+		factionGovernment := determineGovernment(population)
+
+		var relativeStrength string
+		switch result := roll2D(); {
+		case result < 3:
+			relativeStrength = "Obscure group - few have heard of them, no popular support"
+		case result < 6:
+			relativeStrength = "Fringe group - few supporters"
+		case result < 8:
+			relativeStrength = "Minor group - some supporters"
+		case result < 10:
+			relativeStrength = "Notable group - significant support, well known"
+		case result < 12:
+			relativeStrength = "Significant - nearly as powerful as government"
+		default:
+			relativeStrength = "Overwhelming popular support - more powerful than government"
+		}
+
+		factions = append(factions, faction{factionGovernment, relativeStrength})
+	}
+
+	return factions
+}
+
+func determineCulturalDifferences() string {
+	result := strconv.Itoa(int(rollD())) + strconv.Itoa(int(rollD()))
+
+	toInt, err := strconv.ParseUint(result, 10, 8)
+	if err != nil {
+		fmt.Printf("error:", err)
+	}
+
+	culturalDifferences := map[uint8]string{
+		11: "Sexist - one gender is considered subservient or inferior to the other.",
+		12: "Religious - culture is heavily influenced by a religion or belief systems, possibly one unique to this world.",
+		13: "Artistic - art and culture are highly prized. Aesthetic design is important in all artefacts produced on world.",
+		14: "Ritualised – social interaction and trade is highly formalised. Politeness and adherence to traditional forms is considered very important.",
+		15: "Conservative – the culture resists change and outside influences.",
+		16: "Xenophobic – the culture distrusts outsiders and alien influences. Offworlders will face considerable prejudice.",
+		21: "Taboo – a particular topic is forbidden and cannot be discussed. Travellers who unwittingly mention this topic will be ostracised.",
+		22; "Deceptive – trickery and equivocation are considered acceptable. Honesty is a sign of weakness.",
+		23: "Liberal – the culture welcomes change and offworld influence. Travellers who bring new and strange ideas will be welcomed.",
+		24: "Honourable – one’s word is one’s bond in the culture. Lying is both rare and despised.",
+		25: "Influenced – the culture is heavily influenced by another, neighbouring world. Roll again for a cultural quirk that has been inherited from the culture.",
+		26: "Fusion – the culture is a merger of two distinct cultures. Roll again twice to determine the quirks inherited from these cultures. If the quirks are incompatible, then the culture is likely divided.",
+		31: "Barbaric – physical strength and combat prowess are highly valued in the culture. Travellers may be challenged to a fight or dismissed if they seem incapable of defending themselves. Sports tend towards the bloody and violent.",
+		32: "Remnant – the culture is a surviving remnant of a once-great and vibrant civilisation, clinging to its former glory. The world is filled with crumbling ruins and every story revolves around the good old days.",
+		33: "Degenerate – the culture is falling apart and is on the brink of war or economic collapse. Violent protests are common and the social order is decaying.",
+		34: "Progressive – the culture is expanding and vibrant. Fortunes are being made in trade; science is forging bravely ahead.",
+		35: "Recovering – a recent trauma, such as a plague, war, disaster or despotic regime has left scars on the culture.",
+		36: "Nexus – members of many different cultures and species visit here.",
+		41: "Tourist Attraction – some aspect of the culture or the planet draws visitors from all over civilised space.",
+		42: "Violent – physical conflict is common, taking the form of duels, brawls or other contests. Trial by combat is a part of their judicial system.",
+		43: "Peaceful – physical conflict is almost unheard- of. The culture produces few soldiers and diplomacy reigns supreme. Forceful Travellers will be ostracised.",
+		44: "Obsessed – everyone is obsessed with or addicted to a substance, personality, act or item. This monomania pervades every aspect of the culture.",
+		45: "Fashion – fine clothing and decoration are considered vitally important in the culture. Underdressed Travellers have no standing here.",
+		46: "At war – the culture is at war, either with another planet or polity, or is troubled by terrorists or rebels.",
+		51: "Unusual Custom: Offworlders – space travellers hold a unique position in the culture’s mythology or beliefs and travellers will be expected to live up to these myths.",
+		52: "Unusual Custom: Starport – the planet’s starport is more than a commercial centre; it might be a religious temple or be seen as highly controversial and surrounded by protestors.",
+		53: "Unusual Custom: Media – news agencies and telecommunications channels are especially strange here. Getting accurate information may be difficult.",
+		54: "Unusual Customs: Technology – the culture interacts with technology in an unusual way. Telecommunications might be banned, robots might have civil rights or cyborgs might be property.",
+		55: "Unusual Customs: Lifecycle – there might be a mandatory age of termination or anagathics might be widely used. Family units might be different, with children being raised by the state or banned in favour of cloning.",
+		56: "Unusual Customs: Social Standings – the culture has a distinct caste system. Travellers of a low social standing who do not behave appropriately will face punishment.",
+		61: "Unusual Customs: Trade – the culture has an odd attitude towards some aspect of commerce, which may interfere with trade at the spaceport. For example, merchants might expect a gift as part of a deal or some goods may only be handled by certain families.",
+		62: "Unusual Customs: Nobility – those of high social standing have a strange custom associated with them; perhaps nobles are blinded, must live in gilded cages or only serve for a single year before being exiled.",
+		63: "Unusual Customs: Sex – the culture has an unusual attitude towards intercourse and reproduction. Perhaps cloning is used instead or sex is used to seal commercial deals.",
+		64: "Unusual Customs: Eating – food and drink occupies an unusual place in the culture. Perhaps eating is a private affair or banquets and formal dinners are seen as the highest form of politeness.",
+		65: "Unusual Customs: Travel – Travellers may be distrusted or feted, or perhaps the culture frowns on those who leave their homes.",
+		66: "Unusual Custom: Conspiracy – something strange is going on. The government is being subverted by another group or agency.",
+	}
+}
+
 func main() {
 	cont := true
 
 	for cont {
 		planetSize := determinePlanetSize()
 		atmosphere := determineAtmosphere(planetSize.Id)
-		Hydrographics := determineHydrographics(atmosphere.Id)
+		hydrographics := determineHydrographics(atmosphere.Id)
+		temperature := determineTemperature(atmosphere.Id)
+		population := determinePopulation()
+		government := determineGovernment(population.Id)
+		factions := determineFactions(government.Id, population.Id)
 
 		fmt.Println("Would you like to continue? yes/no?")
 
